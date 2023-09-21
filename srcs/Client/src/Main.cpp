c1b10ced7e5243c7bafd6febb1e34f03862ffba3
Maxime Eng <maxime.eng@epitech.eu>
2023-09-21 12:46:06 +0200

    [add] Background moves

--- srcs/Client/src/Main.cpp
+++ srcs/Client/src/Main.cpp
@@ -10,9 +10,10 @@ int main(int argc, const char *argv[]) {
     if (!textureSpace.loadFromFile("./Release/assets/Backgrounds/Space.png"))
         return EXIT_FAILURE;
     sf::Sprite Space(textureSpace);
-    sf::IntRect SpaceToDisplay(0, 0, 1587, 200);
+    sf::IntRect SpaceToDisplay(0, 0, 950, 200);
     Space.setTextureRect(SpaceToDisplay);
     Space.setScale(3, 3);
+    float spaceScrollSpeed = 1.0f;
 
     sf::Texture texture;
     if (!texture.loadFromFile("./Release/assets/sprites/ship.gif"))
@@ -22,6 +23,8 @@ int main(int argc, const char *argv[]) {
     Ship.setTextureRect(ShipToDisplay);
     Ship.setScale(2, 2);
 
+    sf::Clock clock;
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -30,6 +33,18 @@ int main(int argc, const char *argv[]) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        sf::Time elapsed = clock.restart();
+
+        float spaceScrollDistance = spaceScrollSpeed * elapsed.asSeconds();
+        SpaceToDisplay.left += spaceScrollDistance;
+
+        SpaceToDisplay.left += spaceScrollSpeed;
+        if (SpaceToDisplay.left >= 950)
+            SpaceToDisplay.left = 0;
+
+        Space.setTextureRect(SpaceToDisplay);
+
+        window.clear();
         window.draw(Space);
         window.draw(Ship);
         window.display();
