99a9459896f9801cd8ef2cb1cecea6d6f805cc05
Maxime Eng <maxime.eng@epitech.eu>
2023-09-21 16:13:59 +0200

    [add] Ship movement

--- srcs/Client/src/Main.cpp
+++ srcs/Client/src/Main.cpp
@@ -1,10 +1,11 @@
 #include "Client.hpp"
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <iostream>
 
 int main(int argc, const char *argv[]) {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
+    window.setFramerateLimit(60);
     sf::Texture textureSpace;
 
     if (!textureSpace.loadFromFile("./Release/assets/Backgrounds/Space.png"))
@@ -19,11 +20,9 @@ int main(int argc, const char *argv[]) {
     if (!texture.loadFromFile("./Release/assets/sprites/ship.gif"))
         return EXIT_FAILURE;
     sf::Sprite Ship(texture);
-    sf::IntRect ShipToDisplay(75, 50, 25, 20);
+    sf::IntRect ShipToDisplay(66.4, 51.6, 33.2, 17.2);
     Ship.setTextureRect(ShipToDisplay);
     Ship.setScale(2, 2);
-
-    sf::Clock clock;
 
     while (window.isOpen())
     {
@@ -33,17 +32,36 @@ int main(int argc, const char *argv[]) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        sf::Time elapsed = clock.restart();
-
-        float spaceScrollDistance = spaceScrollSpeed * elapsed.asSeconds();
-        SpaceToDisplay.left += spaceScrollDistance;
 
         SpaceToDisplay.left += spaceScrollSpeed;
         if (SpaceToDisplay.left >= 950)
             SpaceToDisplay.left = 0;
-
         Space.setTextureRect(SpaceToDisplay);
 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            Ship.setPosition(Ship.getPosition().x - 1, Ship.getPosition().y);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            Ship.setPosition(Ship.getPosition().x + 1, Ship.getPosition().y);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            Ship.setPosition(Ship.getPosition().x, Ship.getPosition().y - 1);
+            ShipToDisplay.left += 33.2;
+            if (ShipToDisplay.left >= 132.8)
+                ShipToDisplay.left = 132.8;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+            Ship.setPosition(Ship.getPosition().x, Ship.getPosition().y + 1);
+            ShipToDisplay.left -= 33.2;
+            if (ShipToDisplay.left <= 0)
+                ShipToDisplay.left = 0;
+        }
+        Ship.setTextureRect(ShipToDisplay);
+        ShipToDisplay.left = 66.4;
         window.clear();
         window.draw(Space);
         window.draw(Ship);
