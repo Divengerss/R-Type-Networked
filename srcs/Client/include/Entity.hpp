9c26522099bfd23eff3bf32c6cc6272d5a3b564b
Maxime Eng <maxime.eng@epitech.eu>
2023-09-22 15:44:36 +0200

    [create] player class, rewrite main

---
+++ srcs/Client/include/Entity.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2023
+** r-type
+** File description:
+** Entity
+*/
+
+#ifndef ENTITY_HPP_
+#define ENTITY_HPP_
+
+class Entity {
+    public:
+        Entity();
+        ~Entity();
+
+    protected:
+    private:
+};
+
+#endif /* !ENTITY_HPP_ */
--- srcs/Client/src/Main.cpp
+++ srcs/Client/src/Main.cpp
@@ -2,12 +2,13 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <iostream>
+#include "Player.hpp"
 
 int main(int argc, const char *argv[]) {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
     window.setFramerateLimit(60);
-    sf::Texture textureSpace;
 
+    sf::Texture textureSpace;
     if (!textureSpace.loadFromFile("./Release/assets/Backgrounds/Space.png"))
         return EXIT_FAILURE;
     sf::Sprite Space(textureSpace);
@@ -15,15 +16,7 @@ int main(int argc, const char *argv[]) {
     Space.setTextureRect(SpaceToDisplay);
     Space.setScale(3, 3);
     float spaceScrollSpeed = 1.0f;
-
-    sf::Texture texture;
-    if (!texture.loadFromFile("./Release/assets/sprites/ship.gif"))
-        return EXIT_FAILURE;
-    sf::Sprite Ship(texture);
-    sf::IntRect ShipToDisplay(66.4, 51.6, 33.2, 17.2);
-    Ship.setTextureRect(ShipToDisplay);
-    Ship.setScale(2, 2);
-
+    Player Ship;
     while (window.isOpen())
     {
         sf::Event event;
@@ -37,34 +30,11 @@ int main(int argc, const char *argv[]) {
         if (SpaceToDisplay.left >= 950)
             SpaceToDisplay.left = 0;
         Space.setTextureRect(SpaceToDisplay);
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        {
-            Ship.setPosition(Ship.getPosition().x - 1, Ship.getPosition().y);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        {
-            Ship.setPosition(Ship.getPosition().x + 1, Ship.getPosition().y);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        {
-            Ship.setPosition(Ship.getPosition().x, Ship.getPosition().y - 1);
-            ShipToDisplay.left += 33.2;
-            if (ShipToDisplay.left >= 132.8)
-                ShipToDisplay.left = 132.8;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-        {
-            Ship.setPosition(Ship.getPosition().x, Ship.getPosition().y + 1);
-            ShipToDisplay.left -= 33.2;
-            if (ShipToDisplay.left <= 0)
-                ShipToDisplay.left = 0;
-        }
-        Ship.setTextureRect(ShipToDisplay);
-        ShipToDisplay.left = 66.4;
+        Ship.Movement();
+        Ship.getSprite().setTextureRect(Ship.getShipToDisplay());
         window.clear();
         window.draw(Space);
-        window.draw(Ship);
+        window.draw(Ship.getSprite());
         window.display();
     }
     return EXIT_SUCCESS;
