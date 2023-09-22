9148c5048683b7aca41eab329b9e1fc95da91f2f
Maxime Eng <maxime.eng@epitech.eu>
2023-09-22 16:26:16 +0200

    [create] calss Background and rewrite main

--- srcs/Client/src/Main.cpp
+++ srcs/Client/src/Main.cpp
@@ -3,19 +3,22 @@
 #include <SFML/Audio.hpp>
 #include <iostream>
 #include "Player.hpp"
+#include "Background.hpp"
 
 int main(int argc, const char *argv[]) {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
     window.setFramerateLimit(60);
 
-    sf::Texture textureSpace;
-    if (!textureSpace.loadFromFile("./Release/assets/Backgrounds/Space.png"))
-        return EXIT_FAILURE;
-    sf::Sprite Space(textureSpace);
-    sf::IntRect SpaceToDisplay(0, 0, 950, 200);
-    Space.setTextureRect(SpaceToDisplay);
-    Space.setScale(3, 3);
-    float spaceScrollSpeed = 1.0f;
+    // sf::Texture textureSpace;
+    // if (!textureSpace.loadFromFile("./Release/assets/Backgrounds/Space.png"))
+    //     return EXIT_FAILURE;
+    // sf::Sprite Space(textureSpace);
+    // sf::IntRect SpaceToDisplay(0, 0, 950, 200);
+    // Space.setTextureRect(SpaceToDisplay);
+    // Space.setScale(3, 3);
+    // float spaceScrollSpeed = 1.0f;
+
+    Background Space;
     Player Ship;
     while (window.isOpen())
     {
@@ -26,14 +29,15 @@ int main(int argc, const char *argv[]) {
                 window.close();
         }
 
-        SpaceToDisplay.left += spaceScrollSpeed;
-        if (SpaceToDisplay.left >= 950)
-            SpaceToDisplay.left = 0;
-        Space.setTextureRect(SpaceToDisplay);
+        // SpaceToDisplay.left += spaceScrollSpeed;
+        // if (SpaceToDisplay.left >= 950)
+        //     SpaceToDisplay.left = 0;
+        // Space.setTextureRect(SpaceToDisplay);
+        Space.BackgroundScrolling();
         Ship.Movement();
         Ship.getSprite().setTextureRect(Ship.getShipToDisplay());
         window.clear();
-        window.draw(Space);
+        window.draw(Space.getSprite());
         window.draw(Ship.getSprite());
         window.display();
     }
