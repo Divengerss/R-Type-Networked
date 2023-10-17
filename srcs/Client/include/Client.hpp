153c7a28bde926c823fd3fed4d85c79f0ea35c94
Julian EMERY <julian.emery@epitech.eu>
2023-10-18 01:02:38 +0200

    [ADD] System to create Texture component

--- srcs/Client/include/Client.hpp
+++ srcs/Client/include/Client.hpp
@@ -49,14 +49,34 @@ namespace rtype
             }
             ~Game() = default;
 
+            void textureSystem() {
+                auto positions = _reg.get_components<Position>();
+                auto controllable = _reg.get_components<Controllable>();
+                auto textures = _reg.get_components<Texture>();
+                for (std::size_t i = 0; i < positions.size(); i++) {
+                    auto &texture = textures[i];
+                    auto &pos = positions[i];
+                    auto &cont = controllable[i];
+                    if (pos && cont && !texture.has_value()) {
+                        _reg.add_component<Texture>(Entity(i), {"./Release/assets/sprites/r-typesheet42.gif", 66, createdPlayers * 18, 33, 17});
+                        _reg.add_component<Scale>(Entity(i), {3, 3});
+                        createdPlayers++;
+                    }
+                    else if (pos && !cont.has_value() && !texture.has_value()) {
+                        _reg.add_component<Texture>(Entity(i), {"./Release/assets/sprites/r-typesheet5.gif", 233, 0, 33, 36});
+                        _reg.add_component<Scale>(Entity(i), {3, 3});
+                    }
+                }
+            }
+
             void updateSprite()
             {
+                textureSystem();
                 posSys.positionSystemClient(_reg, _client);
                 auto positions = _reg.get_components<Position>();
                 auto velocities = _reg.get_components<Velocity>();
                 auto textures = _reg.get_components<Texture>();
                 auto scales = _reg.get_components<Scale>();
-
                 for (std::size_t i = 0; i < textures.size(); ++i)
                 {
                     auto &texture = textures[i];
@@ -65,11 +85,15 @@ namespace rtype
                     if (texture && pos && _sprites.find(i) == _sprites.end())
                     {
                         sf::Sprite sprite;
-                        sf::Texture spriteTexture(texture->_texture);
-                        sprite.setTexture(spriteTexture);
+                        sf::Texture spriteTexture = sf::Texture();
+                        spriteTexture.loadFromFile(texture->_path);
                         sprite.setPosition(pos->_x, pos->_y);
                         sprite.setTextureRect(sf::IntRect(texture->_left, texture->_top, texture->_width, texture->_height));
                         sprite.setScale(scale->_scaleX, scale->_scaleY);
+                        std::cout << texture->_path << std::endl;
+                        std::cout << pos->_x << " " << pos->_y << std::endl;
+                        std::cout << texture->_left << " " << texture->_top << " " << texture->_width << " " << texture->_height << std::endl;
+                        std::cout << scale->_scaleX << " " << scale->_scaleY << std::endl;
                         _sprites.emplace(i, std::pair<sf::Sprite, sf::Texture>(sprite, spriteTexture));
                         _sprites[i].first.setTexture(_sprites[i].second);
                     } else if (texture && pos) {
@@ -163,6 +187,7 @@ namespace rtype
                 DamageSystem dmgSys;
                 float _updateInterval;
                 Registry &_reg;
+                int createdPlayers = 0;
     };
 } // namespace rtype
 
