27aa4f72c78be80fb0847f005ae22549ed9c40b3
Maxime Eng <maxime.eng@epitech.eu>
2023-10-30 16:29:37 +0100

    feat : background correctly set
     Changes to be committed:
    	modified:   srcs/Runner/src/Main.cpp

--- srcs/Runner/src/Main.cpp
+++ srcs/Runner/src/Main.cpp
@@ -9,49 +9,69 @@
 #include <SDL2/SDL_image.h>
 #include "Registry.hpp"
 #include "Texture.hpp"
+#include "Scale.hpp"
+#include "Position.hpp"
+#include "MovementPattern.hpp"
+#include "Velocity.hpp"
+// #include "PositionSystem.hpp"
 
-int main(int argc, char* argv[]) {
+int main() {
+    // PositionSystem posSys;
     Registry reg;
     reg.register_component<Texture>();
+    reg.register_component<Scale>();
+    reg.register_component<Position>();
+    reg.register_component<MovementPattern>();
+    reg.register_component<Velocity>();
 
     Entity Space_background = reg.spawn_entity();
     reg.add_component<Texture>(Space_background, {"./assets/sprites/Space.png", 0, 0, 950, 200});
+    reg.add_component<Scale>(Space_background, {5, 5});
+    reg.add_component<Position>(Space_background, {0, 0});
+    reg.add_component<MovementPattern>(Space_background, {STRAIGHTLEFT});
+    reg.add_component<Velocity>(Space_background, {1});
 
     auto textures = reg.get_components<Texture>();
+    auto scales = reg.get_components<Scale>();
+    auto positions = reg.get_components<Position>();
+    auto patterns = reg.get_components<MovementPattern>();
+    auto velocities = reg.get_components<Velocity>();
+
     auto &texture = textures[0];
-    std::cout << texture->_path << std::endl;
-    // Initialisation de SDL
+    auto &scale = scales[0];
+    auto &position = positions[0];
+    auto &pattern = patterns[0];
+    auto &velocity = velocities[0];
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         SDL_Log("Erreur lors de l'initialisation de SDL : %s", SDL_GetError());
-        return 1;
+        return 84;
     }
 
-    // Création de la fenêtre
     SDL_Window* window = SDL_CreateWindow("SDL2 Runner", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1920, 1080, SDL_WINDOW_SHOWN);
     if (window == NULL) {
         SDL_Log("Erreur lors de la création de la fenêtre : %s", SDL_GetError());
-        return 2;
+        return 84;
     }
 
-    // Initialisation du sous-système de chargement d'images (SDL_image)
     if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG) {
         SDL_Log("Erreur lors de l'initialisation de SDL_image : %s", IMG_GetError());
-        return 3;
+        return 84;
     }
 
-    // Chargement d'une image
     SDL_Surface* space = IMG_Load(texture->_path.c_str());
     if (space == NULL) {
         SDL_Log("Erreur lors du chargement de l'image : %s", IMG_GetError());
-        return 4;
+        return 84;
     }
 
-    // Création d'une texture à partir de l'image
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     SDL_Texture* imageTexture = SDL_CreateTextureFromSurface(renderer, space);
+    SDL_Rect srcRect = { texture->_left, texture->_top, texture->_width, texture->_height };
+    SDL_Rect destRect = { 0, 0, static_cast<int>(texture->_width * scale->_scaleX), static_cast<int>(texture->_height * scale->_scaleY) };
+
     SDL_FreeSurface(space);
 
-    // Boucle de jeu
     bool isRunning = true;
     SDL_Event event;
     while (isRunning) {
@@ -60,20 +80,16 @@ int main(int argc, char* argv[]) {
                 isRunning = false;
             }
         }
-
-        // Effacement de l'écran
+        // posSys.positionSystemClient(reg);
         SDL_RenderClear(renderer);
 
-        // Affichage de l'image
-        SDL_RenderCopy(renderer, imageTexture, NULL, NULL);
+        SDL_RenderCopy(renderer, imageTexture, &srcRect, &destRect);
 
-        // Mise à jour de l'affichage
         SDL_RenderPresent(renderer);
 
-        SDL_Delay(16);  // Limiter la boucle de jeu à environ 60 FPS (environ 16 ms par frame)
+        SDL_Delay(16);
     }
 
-    // Libération des ressources
     SDL_DestroyTexture(imageTexture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
